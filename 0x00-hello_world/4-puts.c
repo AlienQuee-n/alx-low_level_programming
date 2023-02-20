94a47d6634c1d98f053cbb9064b13d7a154ae273
windcleaver <estherroberts256@gmail.com>
2023-02-20 06:21:54 -0800

    Nice to meet you betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - print a line using puts code
+ *  main -Print a code line using puts
  *
- *Return: Always 0
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
